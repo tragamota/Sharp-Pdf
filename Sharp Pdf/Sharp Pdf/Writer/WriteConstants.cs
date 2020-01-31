c6c38d603091c532d29f091ab476376f4b92ec9b
Ian van de Poll <ian.vd.poll@gmail.com>
2020-01-31 16:56:07 +0100

    Separate cos constants

---
+++ Sharp Pdf/Sharp Pdf/Writer/WriteConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace SharpPdf.Writer
+{
+    internal class WriteConstants
+    {
+        public const string PdfEncoding = "ISO-8859-1";
+        
+        public const string EndOfFile = "%%EOF";
+        public const string SingleBasedNewLine = "\n";
+        public const string DoubleBasedNewLine = "\r\n";
+        
+        public const string DictionaryOpening = "<<";
+        public const string DictionaryClosing = ">>";
+
+        public const string ArrayOpening = "[";
+        public const string ArrayClosing = "]";
+    }
+}
