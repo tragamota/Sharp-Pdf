d5cf03ec51784235403e50957aa71a129f74da37
Ian van de Poll <ian.vd.poll@gmail.com>
2020-01-31 17:00:12 +0100

    Add global encoding

---
+++ Sharp Pdf/Sharp Pdf/Utils/ByteEncoder.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace SharpPdf.Writer.Document
+{
+    public class ByteEncoder
+    {
+        private static readonly Encoding ExtendedEncoding = Encoding.GetEncoding(WriteConstants.PdfEncoding);
+        private static readonly Encoding TextEncoding = Encoding.BigEndianUnicode;
+        public static byte[] StringEncoding(string text)
+        {
+            return text == null ? null : TextEncoding.GetBytes(text);
+        }
+        
+        public byte[] StandardEncoding(string text)
+        {
+            return text == null ? null : ExtendedEncoding.GetBytes(text);
+        }
+    }
+}
