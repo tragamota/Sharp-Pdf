40dc826dee1b0100cb8faea8b587c58e8854eca5
Ian van de Poll <Ian.vd.poll@gmail.com>
2020-02-02 17:06:11 +0100

    Update to Constant Pdf version

--- Sharp Pdf/Sharp Pdf/Writer/Document/Structure/GeneralHeader.cs
+++ Sharp Pdf/Sharp Pdf/Writer/Document/Structure/GeneralHeader.cs
@@ -5,22 +5,14 @@ namespace SharpPdf.Writer.Document.Structure
 {
     internal class GeneralHeader
     {
-        private const string VersionLine = "%PDF-";
+        private const string VersionLine = "%PDF-1.7";
         private const string BinarySignature = "%¼½¾¿";
-
-        private PdfVersion _version;
-
-        public GeneralHeader(PdfVersion version)
-        {
-            _version = version;
-        }
-
+        
         public override string ToString()
         {
             StringBuilder headerBuilder = new StringBuilder();
 
-            headerBuilder.Append(VersionLine);
-            headerBuilder.AppendLine(_version.ToString());
+            headerBuilder.AppendLine(VersionLine);
             headerBuilder.Append(BinarySignature);
 
             return headerBuilder.ToString();
